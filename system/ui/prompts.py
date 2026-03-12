c15fca06c3ee39299e467b06f80b0361584043e9
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-12 15:58:04 -0300

    refactor: extract get_choice from AppController to Prompt class

---
+++ system/ui/prompts.py
@@ -0,0 +1,15 @@
+class Prompt:
+
+    @staticmethod
+    def get_choice(self, valid_options: list) -> int:
+        choice = None
+
+        while choice not in valid_options:
+            try:
+                choice = int(input("Choose an option: "))
+                if choice not in valid_options:
+                    print("\nChoose a valid option\n")
+            except ValueError:
+                print("\nChoose a valid option\n")
+
+        return choice
